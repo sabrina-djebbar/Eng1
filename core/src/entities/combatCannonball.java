f54e7694df5c2f019029a228937c47611066f2e6
shadowbyte4096 <sal3zs2@littleover.derby.sch.uk>
2022-01-31 19:21:42 +0000

    Updated Combat
    Fixed issues
    Added Gold
    Added Time
    Left boundary checks for time and health as idk what you want them to do.
    Also left out background.

--- core/src/entities/combatCannonball.java
+++ core/src/entities/combatCannonball.java
@@ -1,18 +1,47 @@
 package entities;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
-public class combatCannonball extends ApplicationAdapter {
-    private Texture cannonImage;
-
-    @Override
-
-    public void create() {
-        cannonImage = new Texture(Gdx.files.internal("Cannon_Ball.png"));
+public class combatCannonball extends Entity {
+	private Rectangle rectangle = new Rectangle();
+	private Texture img;
+    private Sprite sprite;
+    private Vector2 pos;
+    //Initialise speed - how fast the combatCannonball sprite moves
+    private float SPEED = 200;
+    
+    public combatCannonball(Vector2 position){
+    	img = new Texture("Combat/cannonball.png");
+    	rectangle.width = img.getWidth();
+        rectangle.height = img.getHeight();
+        sprite = new Sprite(img);
+        pos = position;
     }
-
+    
+    public void render(SpriteBatch batch){
+        sprite.draw(batch);
+    }
+    
+    public void update(){
+        move();
+    }
+    
+    private void move() {
+    	pos.y -= SPEED * Gdx.graphics.getDeltaTime();
+    	sprite.setPosition(pos.x, pos.y);
+    	rectangle.setPosition(pos);
+    }
+    
+    public Rectangle getRectangle(){
+    	return (rectangle);
+    }
+    
+    public Vector2 getPos() {
+    	return (pos);
+    }
 }
-
-
--- core/src/screens/combatScreen.java
+++ core/src/screens/combatScreen.java
@@ -2,13 +2,20 @@ package screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.yorkpiratesgame.io.YorkPirates;
+
+import entities.combatCannonball;
+import entities.combatGold;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -22,56 +29,87 @@ public class combatScreen implements Screen {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private Rectangle pirate;
-    private Array<Rectangle> cannonballs;
-    private long lastDropTime;
+    //List of cannonballs and gold coins
+    private Array<combatCannonball> cannonballs;
+    private Array<combatGold> coins;
+    private long lastcombatCannonballTime;
+    private long randomcombatCannonballTime;
+    private long lastCoinTime;
+    private long randomCoinTime;
+    private long startTime = TimeUtils.millis();
     private Texture pirateImage;
-    private Texture cannonImage;
-    /* game.batch is the main SpriteBatch of the whole game, saves making a new one for each screen
-    * as when the game window is closed they all get disposed. */
+    private int pirateHealth;
+    private int goldCollected;
+    private BitmapFont pirateFont;
+    private Color fontColour;
 
     public combatScreen(YorkPirates game){
         this.game = game;
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, game.WIDTH, game.HEIGHT);
-        batch = new SpriteBatch();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        
         pirateImage = new Texture(Gdx.files.internal("Combat/pirateBarrel.png"));
-        cannonImage = new Texture(Gdx.files.internal("Combat/cannonball.png"));
         pirate = new Rectangle();
-        pirate.x = game.WIDTH / 2 - 256 / 2;
+        pirate.x = Gdx.graphics.getWidth() / 2 - 256 / 2;
         pirate.y = 20;
-        pirate.width = 256;
-        pirate.height = 256;
+        pirate.width = pirateImage.getWidth();
+        pirate.height = pirateImage.getHeight();
+        pirateHealth = 100;
+        goldCollected = 0;
 
-        cannonballs = new Array<Rectangle>();
-        spawnCannonball();
+        cannonballs = new Array<combatCannonball>();
+        spawncombatCannonball();
+        coins = new Array<combatGold>();
+        spawnCoin();
     }
 
-    public void spawnCannonball() {
-        Rectangle cannonball = new Rectangle();
-        cannonball.x = MathUtils.random(0, 800-64);
-        cannonball.y = 480;
-        cannonball.width = 64;
-        cannonball.height = 64;
-        cannonballs.add(cannonball);
-        lastDropTime = TimeUtils.nanoTime();
+    public void spawncombatCannonball() {
+    	int x = MathUtils.random(0, Gdx.graphics.getWidth()-64);
+    	int y = Gdx.graphics.getHeight()-20;
+    	Vector2 position = new Vector2 (x, y);
+    	combatCannonball cannonball = new combatCannonball(position);
+    	cannonballs.add(cannonball);
+    	//record time this cannonball was spawned
+        lastcombatCannonballTime = TimeUtils.nanoTime();
+        //set time for next cannonball
+        randomcombatCannonballTime = MathUtils.random(1000000000, 2000000000);
+    }
+    
+    public void spawnCoin() {
+    	int x = MathUtils.random(0, Gdx.graphics.getWidth()-64);
+    	int y = Gdx.graphics.getHeight()-20;
+    	Vector2 position = new Vector2 (x, y);
+        combatGold coin = new combatGold(position);
+        coins.add(coin);
+        lastCoinTime = TimeUtils.nanoTime();
+        randomCoinTime = MathUtils.random(1000000000, 2000000000);
     }
 
     @Override
     public void show() {
+    	//Create colour for labels
+        fontColour = new Color(59/255f,60/255f,54/255f, 3/2);
+        pirateFont = new BitmapFont(Gdx.files.internal("UI/pirateFont.fnt"));
+        pirateFont.setColor(fontColour);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0.2f, 1);
         camera.update();
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(pirateImage, pirate.x, pirate.y);
-        for(Rectangle cannonball: cannonballs) {
-            batch.draw(cannonImage, cannonball.x, cannonball.y);
+        game.batch.begin();
+        game.batch.draw(pirateImage, pirate.x, pirate.y);
+        //render cannonballs
+        for(combatCannonball cannonball: cannonballs) {
+        	cannonball.render(game.batch);
         }
-        batch.end();
-
+        //render coins
+        for(combatGold coin: coins) {
+        	coin.render(game.batch);
+        }
+        drawUI();
+        game.batch.end();
+        
         if(Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
@@ -83,19 +121,37 @@ public class combatScreen implements Screen {
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) pirate.x += 200 * Gdx.graphics.getDeltaTime();
 
         if(pirate.x < 0) pirate.x = 0;
-        if(pirate.x > 800 - 64) pirate.x = 800 - 64;
+        if(pirate.x > Gdx.graphics.getWidth() - pirate.getWidth()) pirate.x = Gdx.graphics.getWidth() - pirate.getWidth();
 
-        // check if we need to create a new raindrop
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnCannonball();
+        // check if we need to create a new coin or cannonball
+        if(TimeUtils.nanoTime() - lastcombatCannonballTime > randomcombatCannonballTime) spawncombatCannonball();
+        if(TimeUtils.nanoTime() - lastCoinTime > randomCoinTime) spawnCoin();
 
-        for (Iterator<Rectangle> iter = cannonballs.iterator(); iter.hasNext(); ) {
-            Rectangle raindrop = iter.next();
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if(raindrop.y + 64 < 0) iter.remove();
-            if(raindrop.overlaps(pirate)) {
+        for (Iterator<combatCannonball> iter = cannonballs.iterator(); iter.hasNext(); ) {
+            combatCannonball cannonball = iter.next();
+            cannonball.update();
+            if(cannonball.getPos().y + 64 < 0) iter.remove();
+            if(cannonball.getRectangle().overlaps(pirate)) {
                 iter.remove();
+                pirateHealth -= 5;
             }
         }
+        
+        for (Iterator<combatGold> iter = coins.iterator(); iter.hasNext(); ) {
+            combatGold coin = iter.next();
+            coin.update();
+            if(coin.getPos().y + 64 < 0) iter.remove();
+            if(coin.getRectangle().overlaps(pirate)) {
+                iter.remove();
+                goldCollected += 1;
+            }
+        }
+    }
+    
+    public void drawUI(){
+        pirateFont.draw(game.batch, "Gold: " + goldCollected, 5, Gdx.graphics.getHeight() - 5);
+        pirateFont.draw(game.batch, "Health: " + pirateHealth, 5, Gdx.graphics.getHeight() - 40);
+        pirateFont.draw(game.batch, "Time Left: " + String.valueOf(60-((TimeUtils.millis() - startTime)/1000)), 5, Gdx.graphics.getHeight() - 75);
     }
 
     @Override
@@ -120,7 +176,6 @@ public class combatScreen implements Screen {
 
     @Override
     public void dispose() {
-        cannonImage.dispose();
         pirateImage.dispose();
         batch.dispose();
     }
